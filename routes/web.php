ed6651ca8a061e0c37dfa4a2e80460c63aab4373
AfiqRosli <afiq_rosli@live.com>
2021-07-07 22:45:28 +0800

    basic routing to all pages

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,29 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+})->name('index');
+
+Route::name('movie.')->group(function () {
+    Route::get('movie', function () {
+        return view('movie.index');
+    })->name('index');
+});
+
+Route::name('member.')->group(function () {
+    Route::get('member', function () {
+        return view('member.index');
+    })->name('index');
+});
+
+Route::name('lend.')->group(function () {
+    Route::get('lend', function () {
+        return view('lend.index');
+    })->name('index');
+});
+
+Route::name('return.')->group(function () {
+    Route::get('return', function () {
+        return view('return.index');
+    })->name('index');
 });
