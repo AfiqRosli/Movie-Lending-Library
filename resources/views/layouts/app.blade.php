3ee24f1dfa36ec1ad3039831adf434e881a7d12f
AfiqRosli <afiq_rosli@live.com>
2021-07-08 02:54:37 +0800

    app layout hasSection for style, script, & h1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,15 +7,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-    <script defer src="{{ mix('js/app.js') }}"></script>
+
+    @hasSection('style')
+    @yield('style')
+    @endif
 
     <title>Movie Lending Library - @yield('title')</title>
 </head>
 
 <body>
     <div id="app" class="container">
+        @hasSection('h1')
+        <h1 class="text-center my-5">@yield('h1')</h1>
+        @endif
+
         @yield('content')
     </div>
+
+    @hasSection('script')
+    <script src="{{ mix('js/app.js') }}"></script>
+
+    @yield('script')
+    @endif
 </body>
 
 </html>
