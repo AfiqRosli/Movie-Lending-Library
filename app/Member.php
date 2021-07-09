02c7e8667c60cf38002284762db3d7eace3f6f43
AfiqRosli <afiq_rosli@live.com>
2021-07-09 17:35:23 +0800

    Member model init joined table, fillable, & timestamps false

--- app/Member.php
+++ app/Member.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    protected $table = 'members';
+
+    protected $fillable = [
+        'name',
+        'age',
+        'address',
+        'telephone',
+        'identity_number'
+    ];
+
+    public $timestamps = false;
 }
