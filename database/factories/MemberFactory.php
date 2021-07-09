f97964c1510b1299136c5ade62a36989acdac3f2
AfiqRosli <afiq_rosli@live.com>
2021-07-09 22:12:10 +0800

    defining movie & member factories content

--- database/factories/MemberFactory.php
+++ database/factories/MemberFactory.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Enums\MemberState;
+use Illuminate\Support\Arr;
 use Faker\Generator as Faker;
 
 $factory->define(App\Member::class, function (Faker $faker) {
+    $memberStates = MemberState::getValues();
+
     return [
-        //
+        'name' => $faker->name,
+        'age' => $faker->numberBetween(15, 40),
+        'address' => $faker->address,
+        'telephone' => $faker->phoneNumber,
+        'identity_number' => $faker->numerify('##-######'),
+        'date_of_joined' => $faker->dateTime()->format('Y-m-d H:i:s'),
+        'is_active' => Arr::random($memberStates),
+        'created_time' => date('Y-m-d H:i:s')
     ];
 });
--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\Enums\MovieGenre;
+use Illuminate\Support\Arr;
 use Faker\Generator as Faker;
 
 $factory->define(App\Movie::class, function (Faker $faker) {
+    $faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
+
+    $movieGenres = MovieGenre::getValues();
+
     return [
-        //
+        'title' => $faker->movie,
+        'genre' => Arr::random($movieGenres),
+        'released_date' => $faker->dateTime()->format('Y-m-d H:i:s'),
+        'created_time' => date('Y-m-d H:i:s')
     ];
 });
