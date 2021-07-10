625281e4a2b600cdb3e35ab183e98593e7f19ba8
AfiqRosli <afiq_rosli@live.com>
2021-07-10 14:16:01 +0800

    LendController home function return lend.home page

--- app/Http/Controllers/LendController.php
+++ app/Http/Controllers/LendController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class LendController extends Controller
 {
+    public function home() {
+        return view('lend.home');
+    }
+
     /**
      * Display a listing of the resource.
      *
