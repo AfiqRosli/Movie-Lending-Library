96c4410333919420b0b35671dff44919811a8983
AfiqRosli <afiq_rosli@live.com>
2021-07-10 14:13:51 +0800

    root page, change lending link to route lend.home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
 <div class="list-group">
     <a href="{{route('movie.index')}}" class="list-group-item list-group-item-action">Movies</a>
     <a href="{{route('member.index')}}" class="list-group-item list-group-item-action">Members</a>
-    <a href="{{route('lend.index')}}" class="list-group-item list-group-item-action">Lending</a>
+    <a href="{{route('lend.home')}}" class="list-group-item list-group-item-action">Lending</a>
     <a href="{{route('return.index')}}" class="list-group-item list-group-item-action">Returning</a>
 </div>
 @endsection
