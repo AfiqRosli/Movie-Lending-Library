85fa66a13f451d53950a0ef5c9930623433f88e6
AfiqRosli <afiq_rosli@live.com>
2021-07-10 14:14:26 +0800

    lend home page, link to two pages lending movie page & lending records page

---
+++ resources/views/lend/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('title', 'Member')
+
+@section('style')
+<link rel="stylesheet" type="text/css"
+    href="https://cdn.datatables.net/v/bs4/dt-1.10.25/af-2.3.7/b-1.7.1/cr-1.5.4/date-1.1.0/kt-2.6.2/r-2.2.9/rg-1.1.3/rr-1.2.8/sc-2.0.4/sb-1.1.0/sp-1.3.0/sl-1.3.3/datatables.min.css" />
+<link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+@endsection
+
+@section('content')
+
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb mb-5">
+        <li class="breadcrumb-item"><a href="{{ route('index') }}">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Lend</li>
+    </ol>
+</nav>
+
+@section('h1', 'Lending Movies')
+
+<div class="list-group">
+    <a href="{{route('lend.index')}}" class="list-group-item list-group-item-action">Lending Records</a>
+    <a href="{{route('lend.create')}}" class="list-group-item list-group-item-action">Lending a Movie</a>
+</div>
+
+@endsection
