36f4f2007de41057f1287be1f2765211fe35d25a
AfiqRosli <afiq_rosli@live.com>
2021-07-11 12:35:35 +0800

    latenessChargeDisplay helper

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -11,4 +11,23 @@ class Helper
     public static function formatDateDB($date) {
         return date('Y-m-d H:i:s', strtotime($date));
     }
+
+    public static function latenessChargeDisplay($lateness_charge) {
+        // check EXACTLY null
+        if ($lateness_charge === null) {
+            return '-';
+        }
+
+        // check EXACTLY 0
+        if ($lateness_charge === 0) {
+            return '$0.00 BND';
+        }
+
+        if ($lateness_charge > 0) {
+            $inDollars = ($lateness_charge / 100);
+            $dollarsWithCents = number_format((float)$inDollars, 2, '.', '');
+
+            return '$' . $dollarsWithCents . ' BND';
+        }
+    }
 }
