a8e6ece928f751bf07ff3040b3da1925de8ed9c5
AfiqRosli <afiq_rosli@live.com>
2021-07-11 12:32:47 +0800

    fill lateness_charge based on lending date & returned date

--- database/factories/LendFactory.php
+++ database/factories/LendFactory.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Arr;
 use Faker\Generator as Faker;
 
 $factory->define(App\Lend::class, function (Faker $faker) {
-    return [
+    $lend = [
         'movie_id' => function() {
             return factory(App\Movie::class)->create()->id;
         },
@@ -15,4 +15,25 @@ $factory->define(App\Lend::class, function (Faker $faker) {
         'returned_date' => Arr::random([null, new DateTime()]),
         'created_time' => date('Y-m-d H:i:s')
     ];
+
+    $lend['lateness_charge'] = function($lend) {
+
+        if ($lend['returned_date'] <> null) {
+            $interval = date_diff($lend['lending_date'], $lend['returned_date']);
+            $days_diff = (int)$interval->format('%a');
+
+            if ($days_diff > 30) {
+                $days_overdue = $days_diff - 30;
+                $charges_per_day = 50; // 50 cents
+
+                return $days_overdue * $charges_per_day;
+            } else {
+                return 0;
+            }
+        }
+
+        return null;
+    };
+
+    return $lend;
 });
